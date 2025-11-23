f669ed93efa87a8662bf913283f6a734c68f08ef
olkhovskiialekseymail.ru <olkhovskiialeksey@mail.ru>
2025-11-23 21:16:35 +1000

    вайл цикл + исключения + функции

--- 1.py
+++ 1.py
@@ -1,35 +1,40 @@
 class NotInplenentedExeptions(Exception):
     ...
-
+def tonirovka():
+    while True:
+        answer = input("какую вы хотите тониовку: 5 % (1), 10% (2), 15% (3)")
+        s = int(answer)
+        if answer == "1":
+            print("сори нету")
+        if answer == "2":
+            print("сори тоже")
+        if answer == "3":
+            print("да без проблем сделаем")
+        repeat = input("хотите другой процент тонировки?(да/нет)")
+        if repeat.strip().lower() == "нет":
+            print("можно вай нот")
+            break
+def polirovka():
+    choose = input("какую бы вы хотели: глубокую (1), поверхностную (2)")
+    if '1' == choose:
+        print("можем сделать")
+    elif "2" == choose:
+        print("можем прям щас, мастер на месте")
+def moyka():
+    choose = input("какого вида мойку желаете быструю (1), усиленную (2)")
+    if '1' == choose:
+        print("сделаем за 15 минут")
+    elif "2" == choose:
+        raise NotInplenentedExeptions("Мастер сейчас занят")
 print('Добро ожаловать в детейлинг центр')
 choose = input('какую услугу вы бы хотели получить: тонировка (1) , полировка (2), мойка (3)')
 
 try:
     if '1' == choose:
-        while True:
-            answer = input("какую вы хотите тониовку: 5 % (1), 10% (2), 15% (3)")
-            s = int(answer)
-            if answer == "1":
-                print("сори нету")
-            if answer == "2":
-                print("сори тоже")
-            if answer == "3":
-                print("да без проблем сделаем")
-            repeat = input("хотите другой процент тонировки?(да/нет)")
-            if repeat.strip().lower() == "нет":
-                print("можно вай нот")
-                break
+        tonirovka()
     elif "2" == choose:
-        choose = input("какую бы вы хотели: глубокую (1), поверхностную (2)")
-        if '1' == choose:
-            print("можем сделать")
-        elif "2" == choose:
-            print("можем прям щас, мастер на месте")
+        polirovka()
     elif "3" == choose:
-        choose = input("какого вида мойку желаете быструю (1), усиленную (2)")
-        if '1' == choose:
-            print("сделаем за 15 минут")
-        elif "2" == choose:
-            raise NotInplenentedExeptions("Мастер сейчас занят")
+        moyka()
 except NotInplenentedExeptions as error:
     print(f"Извините {error}")
