fa5c92860b4d37f0c9ff96a60e3d2b085694327e
olkhovskiialekseymail.ru <olkhovskiialeksey@mail.ru>
2025-11-23 14:22:10 +1000

    вайл цикл с тонировкой

--- py 2.py
+++ py 2.py
@@ -1,5 +1,14 @@
-print('Добро ожаловать в детейлинг центр')
-choose = input('какую услугу вы бы хотели получить: тонировка (1) , полировка (2), мойка (3)')
-if '1' == choose:
-    choose = input('какую вы хотите тониовку: 5 % (1), 10% (2), 15% (3)')
-    if "1" == choose:
+from itertools import repeat
+
+while True:
+    answer = input("какую вы хотите тониовку: 5 % (1), 10% (2), 15% (3)")
+    if answer == "1":
+        print("сори нету")
+    if answer == "2":
+        print("сори тоже")
+    if answer == "3":
+        print("да без проблем сделаем")
+    repeat = input("хотите другой процент тонировки?(да/нет)")
+    if repeat.strip().lower() == "нет":
+        print("можно вай нот")
+        break
